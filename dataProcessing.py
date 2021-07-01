68655315e1586686e5d353ef0606b61d6bc11086
dorothyaddie <dorothy.grace.a@gmail.com>
2021-07-01 13:47:24 -0400

    creating boolean variables to make a data frame of info of all hand that do not have nail polish, acessories, or irregularities

--- dataProcessing.py
+++ dataProcessing.py
@@ -1,1 +1,7 @@
 import pandas as pd
+
+#creating boolean variables to make a data frame of info of all hand that do not have nail polish, acessories, or irregularities
+noNailPolish = HandInfo[nailPolish] == 0
+noAccessories = HandInfo[accessories] == 0
+noIrregularities = HandInfo[irregularities] == 0
+
