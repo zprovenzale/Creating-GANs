d329b170fd6b3eda8e3206c892399e5c12914fdb
dorothyaddie <dorothy.grace.a@gmail.com>
2021-07-01 13:55:18 -0400

    creating data frame

--- dataProcessing.py
+++ dataProcessing.py
@@ -1,12 +1,15 @@
 import pandas as pd
 
+#creating a data frame 
+handInfo = pd.read_csv('HandInfo - HandInfo.csv')
+
 #creating boolean variables to make a data frame of info of all hand that do not have nail polish, acessories, or irregularities
-noNailPolish = HandInfo[nailPolish] == 0
-noAccessories = HandInfo[accessories] == 0
-noIrregularities = HandInfo[irregularities] == 0
+noNailPolish = handInfo[nailPolish] == 0
+noAccessories = handInfo[accessories] == 0
+noIrregularities = handInfo[irregularities] == 0
 
 #creating a separate data frame that only includes Hand Info for hands that do not have nail polish, accessories of irregularities
-newHandInfo = HandInfo[noNailPolish] & HandInfo[noAccessories] & HandInfo[noIrregularities]
+newHandInfo = handInfo[noNailPolish] & handInfo[noAccessories] & handInfo[noIrregularities]
 
 #accessing a list of all the image names that do not have nail polish, accessories or irregularities
 #this can be used to create a separate file of all the pictures of hands we will actually be using to train our neural network
