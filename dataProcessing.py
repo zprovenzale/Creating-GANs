c6395e5fe3a1e831ac23f48d1239273267d0dc3c
dorothyaddie <dorothy.grace.a@gmail.com>
2021-07-01 13:52:04 -0400

    making a list of image names we will be using to train our model

--- dataProcessing.py
+++ dataProcessing.py
@@ -8,3 +8,6 @@ noIrregularities = HandInfo[irregularities] == 0
 #creating a separate data frame that only includes Hand Info for hands that do not have nail polish, accessories of irregularities
 newHandInfo = HandInfo[noNailPolish] & HandInfo[noAccessories] & HandInfo[noIrregularities]
 
+#accessing a list of all the image names that do not have nail polish, accessories or irregularities
+#this can be used to create a separate file of all the pictures of hands we will actually be using to train our neural network
+handImageNames = newHandInfo[imageName].list()
